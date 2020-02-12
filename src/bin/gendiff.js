d792e8d3aa0b58a89a1d453fc4d1f5c1e9b17453
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2020-02-12 23:57:24 +0200

    Step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-
-let firstConfigFile;
-let secondConfigFile;
+const fs = require('fs');
+const _ = require('lodash');
 
 program
   .version('0.0.1')
@@ -11,10 +10,29 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    firstConfigFile = firstConfig;
-    secondConfigFile = secondConfig;
+    const first = JSON.parse(fs.readFileSync(firstConfig, 'utf8'));
+    const second = JSON.parse(fs.readFileSync(secondConfig, 'utf8'));
+    const unionJson = { ...first, ...second };
+    const result = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, value] of Object.entries(unionJson)) {
+      // result.push(`+ ${key}: ${value}`);
+      if (_.has(first, key) && _.has(second, key)) {
+        if (first[key] === second[key]) {
+          result.push(`    ${key}: ${value}`);
+        } else {
+          result.push(`  - ${key}: ${first[key]}`);
+          result.push(`  + ${key}: ${value}`);
+        }
+      } else if (_.has(first, key) && !_.has(second, key)) {
+        result.push(`  - ${key}: ${first[key]}`);
+      } else {
+        result.push(`  + ${key}: ${key}`);
+      }
+    }
+    console.log('{');
+    console.log(result.join('\n'));
+    console.log('}');
   });
 
 program.parse(process.argv);
-
-console.log(firstConfigFile, secondConfigFile);
