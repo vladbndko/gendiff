3174f773813c5feb436ae6e4b4c56ac1497222a5
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2020-02-15 17:43:29 +0200

    added: parsers

---
+++ src/parsers.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import yaml from 'js-yaml';
+import fs from 'fs';
+
+const getExtName = (file) => path.extname(file);
+
+const parse = (file) => {
+  const extName = getExtName(file);
+  const content = fs.readFileSync(file, 'utf8');
+  switch (extName) {
+    case '.json':
+      return JSON.parse(content);
+    case '.yml':
+    case '.yaml':
+      return yaml.safeLoad(content);
+    default:
+      return content;
+  }
+};
+
+export default parse;
