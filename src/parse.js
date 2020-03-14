d16c940dcba40e68f1d3e9c1c46fb46d8f385488
Vladyslav Bondarenko <vlad.bond05@gmail.com>
2020-03-14 17:50:05 +0200

    src/parse.js#L5

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,10 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default (extension, content) => {
-  const parsers = {
-    json: JSON.parse,
-    yaml: yaml.load,
-    ini: ini.parse,
-  };
-  return parsers[extension](content);
+const parsers = {
+  json: JSON.parse,
+  yaml: yaml.load,
+  ini: ini.parse,
 };
+
+export default (extension, content) => parsers[extension](content);
